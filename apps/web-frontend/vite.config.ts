25828dd49dd38e7022816dfdce3e46e9fe4e4915
nao215912 <naoki3.14159@gmail.com>
2025-10-25 22:17:04 +0900

    add: storybook

--- apps/web-frontend/vite.config.ts
+++ apps/web-frontend/vite.config.ts
@@ -6,7 +6,10 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+   !process.env.STORYBOOK && vueDevTools(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
